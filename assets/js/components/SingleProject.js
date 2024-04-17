9219f0ee67e37ee0a0e3b217dadda1af7a3dd078
Carlos Penaforte <dannhl08@gmail.com>
2024-04-17 15:51:04 -0300

    Fix: ui improvement

--- assets/js/components/SingleProject.js
+++ assets/js/components/SingleProject.js
@@ -51,12 +51,12 @@ export default Infimo.default.defineComponent({
     },
     template: `
         <div
-            class="flex flex-col justify-center p-0 shadow-[0_0_4px_1px_rgba(0,0,0,0.2)] text-gray-900 select-none bg-gray-100 w-[360px] rounded-lg"
+            class="flex flex-col justify-center p-0 shadow-[0_0_4px_1px_rgba(0,0,0,0.2)] text-gray-900 select-none bg-gray-100 w-[294px] rounded-lg"
         >
             <figure class="shink-0 flex items-center justify-center w-full bg-gray-800 rounded-t-lg">
                 <img :src="image || languageToLogoPath(language)" loading="lazy" :alt="title" class="w-[150px] h-[150px] my-4">
             </figure>
-            <h3 class="text-2xl font-semibold text-gray-500 mt-4 mb-2 mx-6">{{ title }}</h3>   
+            <h3 class="text-xl font-semibold text-gray-500 mt-4 mb-2 mx-6">{{ title }}</h3>   
             <p class="text-sm m-0 mx-6 h-[80px]">
                 {{ description }}
             </p>
